cb13dbc22bb410d2675e032136e043b28dfbd3d7
Nils Sohn <sohndotcloud@proton.me>
2024-11-25 00:28:22 -0700

    Added tests for ch3 chg1

--- chapter-3/challenge-1/src/main.c
+++ chapter-3/challenge-1/src/main.c
@@ -16,10 +16,22 @@ int main(int argc, char * argv[argc+1]) {
     int length = sizeof(array) / sizeof(0);
     quicksort_recursion(array, 0, length - 1);
 
+    int max = 0;
     for (int i = 0; i < length; i++) {
         printf("%d ", array[i]);
+        if (max > array[i]) {
+            break;
+        } else {
+            max = array[i];
+        }
     }
     printf("\n");
+
+    if (max != array[length - 1]) {
+        printf("List not sorted\n");
+    } else {
+        printf("List is sorted\n");
+    }
 
     return 0;
 }
